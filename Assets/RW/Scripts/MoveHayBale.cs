717c5099493349b26cca52edab9ea9f211d3d7ad
Sònia Carrancà <sonia.carranca01@estudiant.upf.edu>
2020-04-23 17:59:38 +0200

    Lab
    lab

---
+++ Assets/RW/Scripts/MoveHayBale.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveHayBale : MonoBehaviour
+{
+    public Vector3 moveSpeed;
+    public Space space;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed * Time.deltaTime, space);
+    }
+}
